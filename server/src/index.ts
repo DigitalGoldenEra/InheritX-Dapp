9aa5a2db9e444a0e852765b7c951592e92dcc5de
JayWebtech <jethroadamzy@gmail.com>
2026-02-03 18:58:43 +0100

    fixes

--- server/src/index.ts
+++ server/src/index.ts
@@ -44,7 +44,7 @@ app.use(helmet());
 app.use(cors({
   origin: [
     process.env.FRONTEND_URL || 'http://localhost:3000',
-    'https://waitlist.inheritx.org',
+    'https://waitlist.inheritx.org', 'https://www.waitlist.inheritx.org', 'https://waitlist.inheritx.org/'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
